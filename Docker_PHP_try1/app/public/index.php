a29e9b33769c4bf230dcfc288b0a4f52f79a78d2
Kenny Z <96443135+Kenny-zhty516@users.noreply.github.com>
2023-07-12 20:12:26 -0400

    [update] tryit page now works

--- Docker_PHP_try1/app/public/index.php
+++ Docker_PHP_try1/app/public/index.php
@@ -1,4 +1,18 @@
 <!DOCTYPE html>
+<html>
+<head>
+    <title>My Index Page</title>
+</head>
+<body>
+    <h1>Welcome to My Index Page</h1>
 
+    <?php
+    // PHP code section
+    $message = "Hello, PHP!";
+    $currentDate = date("Y-m-d");
+    ?>
 
-
+    <p><?php echo $message; ?></p>
+    <p>Today's date is <?php echo $currentDate; ?></p>
+</body>
+</html>
